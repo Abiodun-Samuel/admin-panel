7d4f754f8ca4da0d895e7007f4d4796125130614
Abiodun Samuel <abiodunsamyemi@gmail.com>
2022-04-09 06:10:39 +0100

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import "./home.scss";
+const Home = () => {
+  return (
+    <>
+      <div className="home">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+      </div>
+    </>
+  );
+};
+
+export default Home;
