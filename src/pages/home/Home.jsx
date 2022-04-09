50f16888b663a95c8e96fe7cf354baa586450b70
Abiodun Samuel <abiodunsamyemi@gmail.com>
2022-04-09 06:36:28 +0100

    add app layout

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
 import "./home.scss";
+import Sidebar from "../../components/Sidebar/Sidebar";
 const Home = () => {
   return (
     <>
       <div className="home">
         <h1>Lorem ipsum dolor sit amet.</h1>
+        <Sidebar />
       </div>
     </>
   );
